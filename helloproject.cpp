ceafe44778de41a4081144bed352cd8cce1a2ed2
Aid Isakovic <aid.isakovic@bulme.at>
2021-10-06 10:38:00 +0200

    Interactivity

--- helloproject.cpp
+++ helloproject.cpp
@@ -1,8 +1,13 @@
 // my hello programm
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello 4BHEL!" << std::endl;
+	string name;
+	cout << "Hello 4BHEL!" << endl;
+	cout << "Dein Name: " << endl;
+	cin >> name;
+	cout << "Hallo " << name << " aus der 4BHEL !" << endl;
 	return 0;
 }
